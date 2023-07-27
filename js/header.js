c2e32e82924409669f2a1f19cbac25725123a4e7
unknown <manojkumarcs70@gmail.com>
2023-07-27 18:57:47 +0530

    some-changes

--- js/header.js
+++ js/header.js
@@ -1,12 +1,15 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 100){
-            $(".navbar").css({"background-color":"white"});
-            $(".nav-item .nav-link").css({"color":"#9a9a9a"})   
+$(document).ready(function () {
+    $(window).ready(function () {
+        $(".nav-item .nav-link").css({ "color": "white" })
+    })
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $(".navbar").css({ "background-color": "white" });
+            $(".nav-item .nav-link").css({ "color": "#9a9a9a" })
         }
-        else{
-            $(".navbar").css({"background-color":"black"});
-            $(".nav-item .nav-link").css({"color":"white"})   
+        else {
+            $(".navbar").css({ "background-color": "black" });
+            $(".nav-item .nav-link").css({ "color": "white" })
         }
 
     })
