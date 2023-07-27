4b73e5a4a9ec7d21cd3995dd375df6535e201d67
unknown <manojkumarcs70@gmail.com>
2023-07-27 14:18:58 +0530

    header

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > 100){
             $(".navbar").css({"background-color":"white"});
-            $(".nav-item .nav-link").css({"color":"black"})   
+            $(".nav-item .nav-link").css({"color":"#9a9a9a"})   
         }
         else{
             $(".navbar").css({"background-color":"black"});
