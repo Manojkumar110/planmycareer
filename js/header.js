554d49e4d094c5e02c65f20c5043664ab82ed564
unknown <manojkumarcs70@gmail.com>
2023-07-29 11:53:34 +0530

    add-css-in-header

--- js/header.js
+++ js/header.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
     $(window).ready(function () {
-        $(".nav-item .nav-link").css({ "color": "white" })
+        $(".nav-item .nav-link").css({ "color": "white" });
+        $(".navbar").css({ "background-color": "black" });
     })
     $(window).scroll(function () {
         if ($(window).scrollTop() > 100) {
             $(".navbar").css({ "background-color": "white" });
-            $(".nav-item .nav-link").css({ "color": "#9a9a9a" })
+            $(".nav-item .nav-link").css({ "color": "#9a9a9a" });
         }
         else {
             $(".navbar").css({ "background-color": "black" });
-            $(".nav-item .nav-link").css({ "color": "white" })
+           $(".nav-item .nav-link").css({ "color": "white" });
         }
 
     })
